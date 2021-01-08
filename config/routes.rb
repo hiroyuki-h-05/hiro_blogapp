55362e720abd1727d710426b6884f0d5746c04a4
hiyoyuki-h-05 <yokoyama.hiro05@gmail.com>
2021-01-08 17:50:41 +0900

    レイアウトの変更・ topページとaboutページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # homeコントラーラのindexアクションを実行
+  # get '/' => 'home#index'
+
+  # root_pathの設定(/のパス)
+  root to: 'home#index'
+
+  get '/about' => 'home#about'
+
 end
