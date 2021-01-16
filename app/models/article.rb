190213e03174e5b004299e585022cec52d34cb99
hiyoyuki-h-05 <yokoyama.hiro05@gmail.com>
2021-01-16 16:48:59 +0900

    オリジナルバリデーションを追加

--- app/models/article.rb
+++ app/models/article.rb
@@ -9,12 +9,37 @@
 #  updated_at :datetime         not null
 #
 class Article < ApplicationRecord
+
+  # 値が存在するかどうか
   validates :title, presence: true
+
+  # 文字数
+  validates :title, length: { minimum: 2, maximum: 100 }
+
+  # 正規表現
+  validates :title, format: { with: /\A(?!\@)/ }
+
   validates :content, presence: true
+  validates :content, length: { minimum: 10 }
+  validates :content, uniqueness: true
+
+  # 独自のルール
+  validate :validate_title_and_content_length
 
   # 時間を表示するためのインスタンスメソッド
   def display_created_at
     I18n.l(self.created_at, format: :default)
+  end
+
+  private
+
+  # 独自のバリデーション
+  def validate_title_and_content_length
+    char_count = self.title.length + self.content.length
+    unless char_count > 100
+      # エラーメッセージを追加
+      errors.add(:content, '100文字以上で！')
+    end
   end
   
 end
