72afa8f36b32885eee387076979d374cb1827734
hiyoyuki-h-05 <yokoyama.hiro05@gmail.com>
2021-01-16 15:07:55 +0900

    railsの開発環境をよくするために複数のgemを導入

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-
 ActiveRecord::Schema.define(version: 2021_01_09_025902) do
 
   create_table "articles", force: :cascade do |t|
