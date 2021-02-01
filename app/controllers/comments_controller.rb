07e1acea97c9dac1320688b0c0b08d840cb8940e
hiroyuki05 <yokoyama.hiro05@gmail.com>
2021-02-01 15:57:56 +0900

    action-textの導入

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,13 +1,19 @@
 class CommentsController < ApplicationController
 
   def new
+    # ルーティングから:aricle_idの値をparamsで取得
     article = Article.find(params[:article_id])
     @comment = article.comments.build
   end
 
   def create
+
+    # ルーティングから:aricle_idの値をparamsで取得
     article = Article.find(params[:article_id])
+
+    # paramsで取得した:article_idの値をもったコメントインスタンスにcomment_paramsの値を渡してbuild
     @comment = article.comments.build(comment_params)
+
     if @comment.save
       redirect_to article_path(article), notice: 'コメントを追加しました'
     else
