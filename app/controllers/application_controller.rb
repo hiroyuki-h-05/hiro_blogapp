5ac28ff5af3b09df95ae34c3118d5bf8c9161425
hiyoyuki-h-05 <yokoyama.hiro05@gmail.com>
2021-02-11 16:17:28 +0900

    i18nを使った国際化・cssの調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :set_locale
 
   # deviseとActiveDecoratorの相性問題を解決
   def current_user
     ActiveDecorator::Decorator.instance.decorate(super) if super.present?
     super
   end
-  
+
+  # 必ず実行されるメソッド（規約）
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
 end
+
+# /articles?id=1
+  # params[:id] => 1
